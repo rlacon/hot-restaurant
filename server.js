0e2619ac12283df6b5f6e92ead8843aa0b6878c9
Matthew Dague <mattdague@gmail.com>
2019-08-02 10:37:44 -0400

    added links to other pages

--- server.js
+++ server.js
@@ -30,6 +30,14 @@ app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "index.html"));
   });
 
+app.get("/reservation", function(req, res) {
+  res.sendFile(path.join(__dirname, "reservation.html"));
+});
+
+app.get("/tables", function(req, res) {
+  res.sendFile(path.join(__dirname, "tables.html"));
+});
+
 app.get("/api/tables", function(req, res){
     return res.json(tables)
 });
